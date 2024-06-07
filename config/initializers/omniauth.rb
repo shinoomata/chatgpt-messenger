2eacf05227e97e74ad2f0fd0d81aada1801f9cfb
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-07 17:49:37 +0900

    Update OmniAuth configuration and add logging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter2,
-    Rails.application.credentials.twitter[:client_id],
-    Rails.application.credentials.twitter[:client_secret],
-    callback_path: "/auth/twitter2/callback",
-    scope: "tweet.read users.read"
-  end
-  
-  OmniAuth.config.logger = Rails.logger
-  OmniAuth.config.logger.level = Logger::DEBUG
+  provider :twitter2,
+           ENV['TWITTER_CLIENT_ID'],
+           ENV['TWITTER_CLIENT_SECRET'],
+           callback_path: "/auth/twitter2/callback",
+           scope: "tweet.read users.read"
+end
+
+OmniAuth.config.logger = Rails.logger
+OmniAuth.config.logger.level = Logger::DEBUG
