d5049c9e54658a778173a4429e50ea9b44ca1488
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-07 16:30:19 +0900

    Fix duplicated presence key in Diary model

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,6 @@
 class Diary < ApplicationRecord
     belongs_to :user
-    validates :content, presence: { message: "1文字以上入力してください" }, length: { maximum: 200, message: "200文字以下で入力してください"  }
+    validates :content, presence: { message: "1文字以上入力してください" }, length: { maximum: 200, message: "200文字以下で入力してください" }
 
     scope :created_today, ->(user) { where(user: user, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 end
