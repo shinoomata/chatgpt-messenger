6dcd5b64990f1c4700fe01823c62dbcbcf1993d5
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 18:19:04 +0900

    session store

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.allowed_request_methods = %i[get post]
+OmniAuth.config.allowed_request_methods = [get post]
 OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
