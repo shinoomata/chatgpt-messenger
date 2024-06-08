95ba03bb39763fdeda904aa087024b8a04464a5a
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 20:29:27 +0900

    racker

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    if Rails.env.development? && !session[:user_id]
-      fake_create
-    else
-      # ログインページを表示するためのアクション
-    end
   end
 
   def auth_twitter2
@@ -32,13 +27,5 @@ class SessionsController < ApplicationController
 
   def failure
     redirect_to root_path, alert: 'ログインに失敗しました'
-  end
-
-  private
-
-  def fake_create
-    user = User.find_or_create_by(uid: '12345', nickname: 'testuser')
-    session[:user_id] = user.id
-    redirect_to new_diary_path
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,9 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            ENV['TWITTER_CLIENT_SECRET'],
            callback_path: "/auth/twitter2/callback",
            scope: "tweet.read users.read"
-           provider_ignores_state: !Rails.env.production?
 end
 
 OmniAuth.config.logger = Rails.logger
 OmniAuth.config.logger.level = Logger::DEBUG
-OmniAuth.config.allowed_request_methods = [:post, :get]
+
