d3a2dfd4d53630f0095cb77620ab877d7cb48d77
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 14:24:31 +0900

    リダイレクトエラー

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.production?
   provider :twitter2,
            ENV['TWITTER_CLIENT_ID'],
            ENV['TWITTER_CLIENT_SECRET'],
            callback_path: "/auth/twitter2/callback",
            scope: "tweet.read users.read"
+          else
+            provider :twitter2,
+                     Rails.application.credentials.twitter[:client_id],
+                     Rails.application.credentials.twitter[:client_secret],
+                     callback_path: "/auth/twitter2/callback",
+                     scope: "tweet.read users.read"
+          end
 end
 
 OmniAuth.config.logger = Rails.logger
