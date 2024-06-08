2a1da4bf727226e7d7fe502c35f01427bfe41926
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 19:39:03 +0900

    rack

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,10 @@
-OmniAuth.config.allowed_request_methods = [get post]
-OmniAuth.config.silence_get_warning = true
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
   provider :twitter2,
            ENV['TWITTER_CLIENT_ID'],
            ENV['TWITTER_CLIENT_SECRET'],
            callback_path: "/auth/twitter2/callback",
            scope: "tweet.read users.read"
-  else
-  provider :developer unless Rails.env.production?
-  end
+           provider_ignores_state: Rails.env.development?
 end
 
 OmniAuth.config.logger = Rails.logger
