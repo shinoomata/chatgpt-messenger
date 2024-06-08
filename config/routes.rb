3cad790eed6904e630d6e803751c41ee1b82b623
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 14:38:08 +0900

    リダイレクトエラー解消せず

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,5 @@ Rails.application.routes.draw do
   get '/auth/failure', to: 'sessions#failure'
   delete '/logout', to: 'sessions#destroy'
   post '/auth/twitter2', to: 'sessions#auth_twitter2', as: 'auth_twitter2' # 追加
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  if Rails.env.production?
-    namespace :admin do
-      resources :migrations, only: [:index]
-    end
-  end
+  
 end
