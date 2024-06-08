63fda459283d3c405ecb2832abfbde4452edfbbd
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 20:05:45 +0900

    rack

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Postdaiko
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.session_store :cookie_store, key: '_your_app_session'
+    config.session_store :cookie_store, key: '_postdaiko_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     # Configuration for the application, engines, and railties goes here.
