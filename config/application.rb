f30693aa7a5ef15ecf3ccbeb480d96859592acee
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-09 08:21:30 +0900

    CSRF

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 # app/controllers/application_controller.rb
 protect_from_forgery except: :auth_twitter2
-
+protect_from_forgery with: :null_session
 module Postdaiko
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
