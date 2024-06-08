3d5c5f4b5c12544ae2e0f020daf021216dee4356
shino omata <shinoomata@shinonoMacBook-Air.local>
2024-06-08 18:09:49 +0900

    承認がうまくいかん

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+OmniAuth.config.allowed_request_methods = %i[get post]
+OmniAuth.config.silence_get_warning = true
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
   provider :twitter2,
